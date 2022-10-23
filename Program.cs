e42df1a8587eb94fe8d12ec80b4a30352fa1da4e
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-10-23 20:11:13 +0200

    producer / routing

--- Program.cs
+++ Program.cs
@@ -4,23 +4,22 @@ using System.Text;
 
 Console.WriteLine("******** RabbitMQ Producer APP ***********");
 
+// get runtime cosumner binding key from the consumer instance id 
+var instanceID = "MQO8";
 var factory = new ConnectionFactory { HostName = "localhost" };
 var connection = factory.CreateConnection();
 
 using var channel = connection.CreateModel();
+// define the exchange 
+channel.ExchangeDeclare(exchange: "myroutingexchange", type: ExchangeType.Direct);
 
-channel.QueueDeclare(queue: "echoQ",
-    durable: false,
-    exclusive: false,
-    autoDelete: false,
-    arguments: null);
 
 while (true)
 {
     Task.Delay(TimeSpan.FromSeconds(1)).Wait();
     var message = $" time :  {DateTime.Now.ToLongTimeString()}";
     var encodedMessage = Encoding.UTF8.GetBytes(message);
-    channel.BasicPublish("", "echoQ", null, encodedMessage);
+    channel.BasicPublish(exchange: "myroutingexchange", routingKey: $"{instanceID}_BindingKey", null, encodedMessage);
 
     Console.WriteLine($"Producer publish {message}");
 };
